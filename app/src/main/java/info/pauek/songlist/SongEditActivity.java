fea3314bfd0ee573bf1738d78f3bc058f7fdfba6
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-10-25 10:25:54 +0200

    Initial commit

---
+++ app/src/main/java/info/pauek/songlist/SongEditActivity.java
@@ -0,0 +1,13 @@
+package info.pauek.songlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_edit);
+    }
+}
